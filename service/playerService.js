11c33f09dbd66a313c772f84ea0c82ce08c4d43f
toota-toota <dont@bother>
2014-11-17 22:45:40 +0100

    fix error when movie and/or subtitle file contains spaces make 'buffer percentage' configurable

--- service/playerService.js
+++ service/playerService.js
@@ -7,7 +7,6 @@ var
     eventEmitter = new EventEmitter(),
 
     client = new WebTorrent(),
-    thresholdPercentage = 2.00,
     tempPath = '/tmp/piflix',
 
     extractVideoFile = function (torrent, callback) {
@@ -29,57 +28,62 @@ exports.eventEmitter = eventEmitter;
 
 exports.playMagnet = function (magnet_uri, subtitlePath) {
 
-    client.destroy(function (data) { // destroy previous torrent downloads
+    configService.get("bufferPercentage", function (bufferPercentage) {
+        client.destroy(function (data) { // destroy previous torrent downloads
 
+            client.download(magnet_uri, function (torrent) { // start new torrent download
+                extractVideoFile(torrent, function (file) {
+                    eventEmitter.emit('buffered', 0.00);
+                    eventEmitter.emit('downloaded', 0.00);
+                    var readStream = file.createReadStream();
+                    var destinationPath = tempPath + '/' + file.name;
 
-        client.download(magnet_uri, function (torrent) { // start new torrent download
-            extractVideoFile(torrent, function (file) {
-                eventEmitter.emit('buffered', 0.00);
-                eventEmitter.emit('downloaded', 0.00);
-                var readStream = file.createReadStream();
-                var destinationPath = tempPath + '/' + file.name;
+                    var writeStream = fs.createWriteStream(destinationPath);
+                    readStream.pipe(writeStream);
 
-                var writeStream = fs.createWriteStream(destinationPath);
-                readStream.pipe(writeStream);
+                    var bytesOnComplete = file.length;
+                    var bytesReceived = 0;
+                    var playerStarted = false;
 
-                var bytesOnComplete = file.length;
-                var bytesReceived = 0;
-                var playerStarted = false;
+                    readStream.on('data', function (chunk) {
+                        bytesReceived += chunk.length;
+                        var percentage = ((bytesReceived / bytesOnComplete) * 100).toFixed(2);
+                        var relativePercentage = ((percentage / bufferPercentage) * 100).toFixed(2);
 
-                readStream.on('data', function (chunk) {
-                    bytesReceived += chunk.length;
-                    var percentage = ((bytesReceived / bytesOnComplete) * 100).toFixed(2);
-                    var relativePercentage = ((percentage / thresholdPercentage) * 100).toFixed(2);
-
-                    if (!playerStarted) {
-                        if (relativePercentage < 100) {
-                            eventEmitter.emit('buffered', relativePercentage);
-                        } else {
-                            eventEmitter.emit('buffered', 100.00);
-                            configService.get('player', function(player) {
-                                var execBuilder = '';
-                                if(player === 'omx') {
-                                    execBuilder += 'omxplayer -p -o hdmi ';
-                                    execBuilder += destinationPath;
-                                    if(subtitlePath != null) {
-                                        execBuilder += ' --subtitles ';
-                                        execBuilder += subtitlePath;
+                        if (!playerStarted) {
+                            if (relativePercentage < 100) {
+                                eventEmitter.emit('buffered', relativePercentage);
+                            } else {
+                                eventEmitter.emit('buffered', 100.00);
+                                configService.get('player', function (player) {
+                                    var execBuilder = '';
+                                    if (player === 'omx') {
+                                        execBuilder += 'omxplayer -p -o hdmi ';
+                                        execBuilder += "\"" + destinationPath + "\"";
+                                        if (subtitlePath != null) {
+                                            execBuilder += ' --subtitles ';
+                                            execBuilder += "\"" + subtitlePath + "\"";
+                                        }
+                                    } else if (player == 'vlc') {
+                                        console.log('Using vlc');
+                                        execBuilder += '/opt/homebrew-cask/Caskroom/vlc/2.1.5/VLC.app/Contents/MacOS/VLC '
+                                        execBuilder += "\"" + destinationPath + "\"";
+                                        if (subtitlePath != null) {
+                                            execBuilder += ' --sub-file '
+                                            execBuilder += "\"" + subtitlePath + "\"";
+                                        }
                                     }
-                                } else if(player == 'vlc') {
-                                    execBuilder += '/opt/homebrew-cask/Caskroom/vlc/2.1.5/VLC.app/Contents/MacOS/VLC '
-                                    execBuilder += destinationPath;
-                                    if(subtitlePath != null) {
-                                        execBuilder += ' --sub-file '
-                                        execBuilder += subtitlePath;
-                                    }
-                                }
-                                proc.exec(execBuilder);
-                                playerStarted = true;
-                            });
+
+                                    console.log('starting player command:');
+                                    console.log(execBuilder);
+                                    proc.exec(execBuilder);
+                                    playerStarted = true;
+                                });
+                            }
                         }
-                    }
 
-                    eventEmitter.emit('downloaded', percentage);
+                        eventEmitter.emit('downloaded', percentage);
+                    });
                 });
             });
         });
